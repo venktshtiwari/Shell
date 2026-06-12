c0db52b9502490409df6d7937735042433449e0d
venktshtiwari <venkatesh.tiwari.met25@itbhu.ac.in>
2026-06-12 08:59:45 +0530

    Add exit

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,12 @@ int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-    std::cout << "$ ";
     std::string command;
-    std::getline(std::cin, command);
-    std::cout << command << ": command not found" << std::endl;
-
+    std::cout << "$ ";
+    while (std::getline(std::cin, command)) {
+	if (command=="exit") break;
+	std::cout << command << ": command not found" << std::endl;
+	std::cout << "$ ";
+    }
 }
+
