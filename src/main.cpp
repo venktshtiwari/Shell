67727d0fb488d0b6634f84e987b11a0a315d6db9
venktshtiwari <venkatesh.tiwari.met25@itbhu.ac.in>
2026-06-12 09:05:09 +0530

    Add echo

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,12 @@ int main() {
     // Flush after every std::cout / std:cerr
     std::cout << std::unitbuf;
     std::cerr << std::unitbuf;
-    std::string command;
+    std::string input;
     std::cout << "$ ";
-    while (std::getline(std::cin, command)) {
-	if (command=="exit") break;
-	std::cout << command << ": command not found" << std::endl;
+    while (std::getline(std::cin, input)) {
+	if (input=="exit") break;
+	if (input.substr(0,5)=="echo ") std::cout << input.substr(5) << std::endl;
+	else std::cout << input << ": command not found" << std::endl;
 	std::cout << "$ ";
     }
 }
